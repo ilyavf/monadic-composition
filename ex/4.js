bf8485c001a013fbaac106c1a12cf413b6beaa1a
Ilya <fadeev.ilya@gmail.com>
2019-09-11 21:11:08 -0400

    combined ex

---
+++ ex/4.js
@@ -0,0 +1,40 @@
+const add5 = x => [x + 5, '+ 5']
+const double = x => [x * 2, '* 2']
+const deduct3 = x => Promise.resolve([x - 3, '- 3'])
+
+const compose = fns => x => {
+  return fns.reduce((acc, fn) => {
+    if (!(acc instanceof Promise)) {
+      acc = Promise.resolve(acc)
+    }
+    return acc.then(([x, desc]) => {
+      let res = fn(x)
+      if (!(res instanceof Promise)) {
+        res = Promise.resolve(res)
+      }
+      return res.then(([x2, desc2]) => {
+        return [x2, desc + ' ' + desc2]
+      })
+    })
+  }, [x, ''])
+}
+
+const app = compose([
+  add5,
+  double,
+  deduct3
+])
+
+const app2 = compose([
+  app,
+  double
+])
+
+function run(x) {
+  app2(x).then(([y, desc]) => {
+    console.log(x + desc + ' = ' + y)
+    // >>> 5  + 5 * 2 - 3 * 2 = 34
+  })
+}
+
+run(5)
